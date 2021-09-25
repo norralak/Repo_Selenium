500589629e6372de8b04c84994d97ce8b451a0eb
Norralak <nsukaram13@gmail.com>
2021-09-24 23:01:22 -0400

    Web Elements Commit

---
+++ src/main/java/TicketTwo/TicketTwo.java
@@ -0,0 +1,35 @@
+package TicketTwo;
+
+public class TicketTwo {
+    /**
+     * Web Elements
+     * Text Box/Input Box --> Type, clear, click on
+     * Buttons & Radio Buttons --> Click
+     * Check Box --> Click (Check and Uncheck)
+     * Drop Down --> Click/Type
+     * Hyperlinks/Links --> Click to go to another webpage (See below)
+     * Link Text (The actual text associated with the link) --> Click
+     * Image --> Click
+     * Alerts
+     * Frames
+     */
+
+    /**
+     * DOM - Document Object Model
+     * Front-End Code --> DOM --> Creates Website/Webpage
+     * Right-click --> Inspect --> DOM (SOURCE)
+     * <tag> Opening and closing tags. Tag names cannot have spaces. </tag>
+     * <html>
+     *     <head>
+     *         Something Here... Title Back/Forward URL
+     *     </head>
+     *     <body>
+     *         Most of the page's contents
+     *         <p>
+     *             Paragraph tag here.
+     *         </p>
+     *     </body>
+     * </html>
+     * Chropath Plugin/Extension used for web automation in Google Chrome
+     */
+}
